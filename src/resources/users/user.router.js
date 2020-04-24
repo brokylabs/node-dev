c259e5a8a12f4b2723fafc72c81cce564b20074e
ZeroIves <c.meong@gmail.com>
2020-04-25 05:22:12 +0700

    Udate CRUD #3
    CRUD Delete Users

--- src/resources/users/user.router.js
+++ src/resources/users/user.router.js
@@ -2,16 +2,19 @@ import express from 'express'
 import userController from './user.controller'
 export const userRoute = express.Router()
 
-
 // Home route
 userRoute
     .route('/')
     .get(userController.getUsers)
     .post(userController.createUser)
+    // .put
+    // .delete
 
 // Home route with parse ID
 userRoute
     .route('/:id')
+    // .get
+    // .post
     .put(userController.updateUsers)
     .delete(userController.deleteUsers)
 
